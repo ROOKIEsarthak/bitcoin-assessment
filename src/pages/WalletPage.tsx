55406fa277cc878c8a622f71736b9fe9208730c3
Iambivert1 <skashyap@m0.ventures>
2024-05-03 02:23:02 +0530

    feat - redesigning wallet page

--- src/pages/WalletPage.tsx
+++ src/pages/WalletPage.tsx
@@ -1,8 +1,8 @@
 export default function WalletPage() {
   return (
     <div>
-      <div className="flex flex-col min-h-[81.5vh] bg-black text-white items-center justify-center">
-        Wallet Page
+      <div className="flex flex-grid min-h-[81.5vh] bg-black text-white gap-10">
+        <div className="flex items-center justify-center ml-10">Wallets</div>
       </div>
     </div>
   );
